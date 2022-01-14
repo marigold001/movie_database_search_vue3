0c362997f8078c15ad65dd998fdcfc85e79b683a
nevenjo7 <neven.josipovic.sae@gmail.com>
2022-01-14 18:52:22 +0100

    Movie database search using axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,140 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+      <div id="searchField">
+        <i class="fas fa-search"></i>
+        <input type="text" v-model="searchTerm" v-on:keyup="updateList" placeholder="Search...">
+        <select name="type" id="type" v-model="value" v-on:click="changeValue">
+          <option value="&type=movie">Movie</option>
+          <option value="&type=seres">Series</option>
+          <option value="&type=episode">Episode</option>
+        </select>
+      </div>
+      <ul v-if="info">
+        <li v-for="(movie, index) in info" :key="index">
+          <img :src="movie.Poster" alt="Movie poster">
+          <h2>{{movie.Title}} </h2>
+        </li>
+      </ul>
+        <p v-if="error">{{this.error}}</p>
   </div>
 </template>
 
 <script>
+// Imports axios node module from node modules
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      info: null,
+      apiKey: '3154fa4f',
+      searchTerm: '',
+      error: 'Search for something',
+      value: '&type=movie'
+    }
+  },
+  mounted() {
+    
+  },
+  methods: {
+    updateList() {
+        axios.get('https://www.omdbapi.com/?apikey=' + this.apiKey + this.value + '&s=' + this.searchTerm)
+      .then((response) => {
+        if(response.data.Response == "True") {
+          this.info = response.data.Search
+          this.error = null
+        } else {
+          if(response.data.Error == "Something went wrong.") {
+            this.error = "Search for something."
+          } else {
+            this.error = response.data.Error
+          }
+          this.info = null
+        }
+        console.log(response)
+      })
+      .catch(this.error = "Something went wrong.")
+      .then();
+      },
+      changeValue() {
+        if(this.value == "&type=movie") {
+          this.searchTerm = ''
+        }
+        if(this.value == "&type=series") {
+          this.searchTerm = ''
+        }
+        if(this.value == "&type=episode") {
+          this.searchTerm = ''
+        }
+       }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+div#searchField {
+  background: #1BBC9B;
+  width: 900px;
+  margin: 0px auto;
+  border-radius: 5px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+div#searchField i {
+  float: left;
+  padding-top: 10px;
+  padding-left: 10px;
+  line-height: 40px;
+  font-size: 15px;
+}
+div#searchField input {
+  padding: 20px;
+  float: left;
+  font-size: 15px;
+  width: 60%;
+  background: transparent;
+  outline: none;
+  border: none;
+}
+div#searchField input::placeholder {
+  font-size: 15px;
+  color: #000;
+}
+div#searchField select {
+  padding: 20px;
+  font-size: 15px;
+  outline: none;
+  border: none;
+  background: transparent;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+  border-bottom: 1px solid grey;
+  margin: 0px auto;
+  max-width: 900px;
+  list-style-type: none;
+  background: #34495E;
+  color: #fff;
 }
-a {
-  color: #42b983;
+li:hover {
+  background: #2D3E50;
+  color: #1BBC9B;
+}
+li img {
+  height: 100px;
+  float: left;
+  margin-top: 20px;
+  margin-left: 10px;
+}
+h2 {
+  display: inline-block;
+  line-height: 100px;
+  font-size: 30px;
+  max-width: 80%;
+}
+ul {
+  padding: 0px;
+  margin: 0px;
 }
 </style>
